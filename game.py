2220e4acbdb2cad9c1022a5dde033e3f9b2aec18
aegis945 <artiom.lekincev@gmail.com>
2024-10-01 04:36:23 +0300

    add winning condition and score tab

--- game.py
+++ game.py
@@ -32,7 +32,7 @@ class EscapeTheMaze:
         window_height = self.scale * self.height
         window_width = self.scale * self.width
         self.window = pygame.display.set_mode((window_width, window_height + self.scale))
-        self.game_font = pygame.font.SysFont("Arial", 24)
+        self.game_font = pygame.font.SysFont("None", 30)
 
         pygame.display.set_caption("Escape the Maze")
 
@@ -78,6 +78,12 @@ class EscapeTheMaze:
         self.window.fill((255, 255, 255))
         self.draw_maze()
         self.robot.draw(self.window)
+
+        score_text = self.game_font.render(f'Score: {self.score}', True, (255, 255, 255))
+        score_x = self.width * self.scale - score_text.get_width() - 10
+        score_y = 10
+        self.window.blit(score_text, (score_x, score_y))
+
         pygame.display.flip()
 
     def draw_maze(self):
@@ -101,11 +107,25 @@ class EscapeTheMaze:
                     exit()
             if event.type == pygame.QUIT:
                 exit()
+
+    def game_solved(self):
+        message = self.game_font.render("You escaped!", True, (0, 255, 0))
+        message_rect = message.get_rect(center=(self.width * self.scale // 2, self.height * self.scale // 2))
+        self.window.blit(message, (self.scale, self.height * self.scale))
+        pygame.draw.rect(self.window, (255, 255, 255), message_rect.inflate(20, 20))
+        self.window.blit(message, message_rect)
+        
+        pygame.display.flip()
+        pygame.time.wait(2000)
+        exit()
         
     def main_loop(self):
         while True:
             self.check_events()
             self.draw_window()
+
+            if self.map[self.robot.y][self.robot.x] == 2:
+                self.game_solved()
 
 if __name__ == "__main__":
     EscapeTheMaze()
